ee0260f39826ff9180b75eb092c53d2277dd82f0
unknown <muhammadanis2007@gmail.com>
2025-10-06 17:01:00 +0500

    commited code initialy

---
+++ config/db.js
@@ -0,0 +1,1 @@
+const sql = require('mssql'); const config = { user: process.env.DB_USER, password: process.env.DB_PASS, server: process.env.DB_SERVER, database: process.env.DB_NAME, options: { encrypt: true, trustServerCertificate: true } }; module.exports = { sql, config };
---
+++ services/alertsService.js
@@ -0,0 +1,1 @@
+<content from alertsService.js>
